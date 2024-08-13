acb33213bc9820d0fa53f2210ab0088c9920e7e5
tuhin92 <tuhinalmamun71m@gmail.com>
2024-08-13 13:43:00 +0600

    set all routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Service from "../Components/Service";
+import Contact from "../Components/Contact";
+import About from "../Components/About";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +17,14 @@ const router = createBrowserRouter([
         {
             path: 'service',
             element: <Service></Service>
+        },
+        {
+            path: 'contact',
+            element: <Contact></Contact>
+        },
+        {
+            path: 'about',
+            element: <About></About>
         }
       ]
     },
