80f8f6b08ed339f040322d95c10d1b22767facc2
tuhin92 <tuhinalmamun71m@gmail.com>
2024-08-17 23:06:19 +0600

    add schedule and pricing

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { IoIosCheckmarkCircle } from "react-icons/io";
 
 const Pricing = () => {
     return (
-        <div className='max-w-7xl mx-auto'>
+        <div className='max-w-7xl mx-auto my-8'>
             {/* schedule  */}
-            <h2 className='text-5xl font-bold text-center mt-4'>Schedule</h2>
+            <h2 className='text-5xl font-bold text-center '>Schedule</h2>
 
 
             <div className='grid grid-cols-1 lg:grid-cols-2 justify-items-center gap-10  mt-8'>
@@ -36,6 +37,107 @@ const Pricing = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+
+
+
+            {/* pricing  */}
+            <h2 className='text-5xl font-bold text-center my-12'>Pricing</h2>
+            <div className='mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center'>
+                {/* first card  */}
+                <div className="card bg-white w-96 transition-transform transform hover:scale-105 hover:shadow-lg hover:bg-gray-100">
+                    <div className="card-body text-black">
+                        <h2 className="text-center text-4xl">Basic</h2>
+                        <div className='flex items-center justify-center gap-2'>
+                            <h2 className='text-8xl font-semibold'>79</h2>
+                            <h2 className='text-4xl font-semibold'>USD</h2>
+                        </div>
+                        <h2 className='text-4xl text-center'>6 Weeks</h2>
+                        <p className='text-base font-semibold text-center text-[#767575] py-4'>Get started with our basic plan.</p>
+                        <hr className='mt-2 border-[#767575]' />
+
+                        <div className='mt-4'>
+                            <div className='flex items-center gap-3 py-2'>
+                                <IoIosCheckmarkCircle />
+                                <p className='font-semibold'>Diet Plan</p>
+                            </div>
+                            <div className='flex items-center gap-3'>
+                                <IoIosCheckmarkCircle />
+                                <p className='font-semibold'>Workout Plan</p>
+                            </div>
+                            <div className='flex items-center gap-3 py-2'>
+                                <IoIosCheckmarkCircle />
+                                <p className='font-semibold'>Daily Follow-ups</p>
+                            </div>
+                        </div>
+                        <button className="btn border-none bg-[#E6533C] text-white px-8 mt-6">Get Now!</button>
+                    </div>
+                </div>
+
+
+                {/* second card  */}
+                <div className="card bg-[#11a296] w-96 transition-transform transform hover:scale-105 hover:shadow-lg hover:bg-gray-100">
+                    <div className="card-body text-black">
+                        <h2 className="text-center text-4xl">Standard</h2>
+                        <div className='flex items-center justify-center gap-2'>
+                            <h2 className='text-8xl font-semibold'>99</h2>
+                            <h2 className='text-4xl font-semibold'>USD</h2>
+                        </div>
+                        <h2 className='text-4xl text-center'>8 Weeks</h2>
+                        <p className='text-base font-semibold text-center  py-4'>Recommended: Give yourself enough time to fully change.</p>
+                        <hr className='mt-2 border-[#767575]' />
+
+                        <div className='mt-4'>
+                            <div className='flex items-center gap-3 py-2'>
+                                <IoIosCheckmarkCircle />
+                                <p className='font-semibold'>Diet Plan</p>
+                            </div>
+                            <div className='flex items-center gap-3'>
+                                <IoIosCheckmarkCircle />
+                                <p className='font-semibold'>Workout Plan</p>
+                            </div>
+                            <div className='flex items-center gap-3 py-2'>
+                                <IoIosCheckmarkCircle />
+                                <p className='font-semibold'>Daily Follow-ups</p>
+                            </div>
+                        </div>
+                        <button className="btn border-none bg-[#E6533C] text-white px-8 mt-6">Get Now!</button>
+                    </div>
+                </div>
+
+
+                {/* third card  */}
+                <div className="card bg-white w-96 transition-transform transform hover:scale-105 hover:shadow-lg hover:bg-gray-100">
+                    <div className="card-body text-black">
+                        <h2 className="text-center text-4xl">Premium</h2>
+                        <div className='flex items-center justify-center gap-2'>
+                            <h2 className='text-8xl font-semibold'>149</h2>
+                            <h2 className='text-4xl font-semibold'>USD</h2>
+                        </div>
+                        <h2 className='text-4xl text-center'>12 Weeks</h2>
+                        <p className='text-xl text-center text-[#767575] py-4'>Fully commit  to your body with our Premium plan.</p>
+                        <hr className='mt-2 border-[#767575]' />
+
+                        <div className='mt-4'>
+                            <div className='flex items-center gap-3 py-2'>
+                                <IoIosCheckmarkCircle />
+                                <p className='font-semibold'>Diet Plan</p>
+                            </div>
+                            <div className='flex items-center gap-3'>
+                                <IoIosCheckmarkCircle />
+                                <p className='font-semibold'>Workout Plan</p>
+                            </div>
+                            <div className='flex items-center gap-3 py-2'>
+                                <IoIosCheckmarkCircle />
+                                <p className='font-semibold'>Daily Follow-ups</p>
+                            </div>
+                        </div>
+                        <button className="btn border-none bg-[#E6533C] text-white px-8 mt-6">Get Now!</button>
+                    </div>
+                </div>
+
+
+
             </div>
         </div>
     );
